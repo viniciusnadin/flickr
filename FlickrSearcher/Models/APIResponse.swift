ae7400476295511e76f194f24142ea256baa598d
Vinicius Nadin <vininadin@gmail.com>
2024-06-06 00:15:24 -0300

    Implements SearchRepository and APIResponse structure

---
+++ FlickrSearcher/Models/APIResponse.swift
@@ -0,0 +1,12 @@
+//
+//  APIResponse.swift
+//  FlickrSearcher
+//
+//  Created by Vinicius Nadin on 06/06/24.
+//
+
+import Foundation
+
+struct APIResponse: Codable {
+    var items: [Image]
+}
---
+++ FlickrSearcher/Repository/SearchRepository.swift
@@ -0,0 +1,20 @@
+//
+//  SearchRepository.swift
+//  FlickrSearcher
+//
+//  Created by Vinicius Nadin on 06/06/24.
+//
+
+import Foundation
+
+class SearchRepository: SearchRepositoryProtocol {
+    func searchImage(_ text: String) async throws -> [Image] {
+        let tags = text.replacingOccurrences(of: " ", with: ",")
+        let urlString = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags=\(tags)"
+        guard let url = URL(string: urlString) else { throw URLError(.badURL) }
+
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let response = try JSONDecoder().decode(APIResponse.self, from: data)
+        return response.items
+    }
+}
