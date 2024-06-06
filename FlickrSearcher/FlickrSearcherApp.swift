9ab94116cbf91416a43b3a00da52367446129c08
Vinicius Nadin <vininadin@gmail.com>
2024-06-06 00:11:37 -0300

    Initial Commit

---
+++ FlickrSearcher/FlickrSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearcherApp.swift
+//  FlickrSearcher
+//
+//  Created by Vinicius Nadin on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
